35828324c62e81687ef1f826ae60f765ed328364
Azizjan Ayupov <litpulla@mail.ru>
2020-06-22 19:18:21 +0600

    Добавление

--- SHS/Form1.h
+++ SHS/Form1.h
@@ -42,6 +42,12 @@ namespace CppCLRWinformsProjekt {
 	private: System::Windows::Forms::DataGridViewTextBoxColumn^ GivenDate;
 	private: System::Windows::Forms::DataGridViewTextBoxColumn^ GivenPrice;
 	private: System::Windows::Forms::DataGridViewTextBoxColumn^ ActualPrice;
+	private: System::Windows::Forms::Panel^ panel1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::TextBox^ InputPriceTxt;
+	private: System::Windows::Forms::TextBox^ InputNameTxt;
+	private: System::Windows::Forms::Button^ addNewEntryBtn;
 
 
 
@@ -74,17 +80,26 @@ namespace CppCLRWinformsProjekt {
 		/// Erforderliche Methode für die Designerunterstützung.
 		/// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
 		/// </summary>
+		
 	
 
 		void InitializeComponent(void)
 		{
+			goods = gcnew Queue();
 			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
 			this->Id = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
 			this->GoodName = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
 			this->GivenDate = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
 			this->GivenPrice = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
 			this->ActualPrice = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			this->addNewEntryBtn = (gcnew System::Windows::Forms::Button());
+			this->InputNameTxt = (gcnew System::Windows::Forms::TextBox());
+			this->InputPriceTxt = (gcnew System::Windows::Forms::TextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
+			this->panel1->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// dataGridView1
@@ -94,7 +109,7 @@ namespace CppCLRWinformsProjekt {
 				this->Id, this->GoodName,
 					this->GivenDate, this->GivenPrice, this->ActualPrice
 			});
-			this->dataGridView1->Location = System::Drawing::Point(12, 12);
+			this->dataGridView1->Location = System::Drawing::Point(12, 175);
 			this->dataGridView1->Name = L"dataGridView1";
 			this->dataGridView1->Size = System::Drawing::Size(598, 150);
 			this->dataGridView1->TabIndex = 0;
@@ -134,37 +149,90 @@ namespace CppCLRWinformsProjekt {
 			this->ActualPrice->Name = L"ActualPrice";
 			this->ActualPrice->ReadOnly = true;
 			// 
+			// panel1
+			// 
+			this->panel1->Controls->Add(this->label2);
+			this->panel1->Controls->Add(this->label1);
+			this->panel1->Controls->Add(this->InputPriceTxt);
+			this->panel1->Controls->Add(this->InputNameTxt);
+			this->panel1->Controls->Add(this->addNewEntryBtn);
+			this->panel1->Location = System::Drawing::Point(13, 13);
+			this->panel1->Name = L"panel1";
+			this->panel1->Size = System::Drawing::Size(337, 156);
+			this->panel1->TabIndex = 1;
+			// 
+			// addNewEntryBtn
+			// 
+			this->addNewEntryBtn->Location = System::Drawing::Point(116, 55);
+			this->addNewEntryBtn->Name = L"addNewEntryBtn";
+			this->addNewEntryBtn->Size = System::Drawing::Size(75, 23);
+			this->addNewEntryBtn->TabIndex = 0;
+			this->addNewEntryBtn->Text = L"Add";
+			this->addNewEntryBtn->UseVisualStyleBackColor = true;
+			this->addNewEntryBtn->Click += gcnew System::EventHandler(this, &Form1::addNewEntryBtn_Click);
+			// 
+			// InputNameTxt
+			// 
+			this->InputNameTxt->Location = System::Drawing::Point(91, 3);
+			this->InputNameTxt->Name = L"InputNameTxt";
+			this->InputNameTxt->Size = System::Drawing::Size(100, 20);
+			this->InputNameTxt->TabIndex = 1;
+			// 
+			// InputPriceTxt
+			// 
+			this->InputPriceTxt->Location = System::Drawing::Point(91, 29);
+			this->InputPriceTxt->Name = L"InputPriceTxt";
+			this->InputPriceTxt->Size = System::Drawing::Size(100, 20);
+			this->InputPriceTxt->TabIndex = 2;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(50, 10);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(35, 13);
+			this->label1->TabIndex = 3;
+			this->label1->Text = L"Name";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(24, 36);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(61, 13);
+			this->label2->TabIndex = 4;
+			this->label2->Text = L"Given price";
+			// 
 			// Form1
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(622, 214);
+			this->ClientSize = System::Drawing::Size(622, 337);
+			this->Controls->Add(this->panel1);
 			this->Controls->Add(this->dataGridView1);
 			this->Name = L"Form1";
 			this->Text = L"Second Hand Shop";
 			this->Load += gcnew System::EventHandler(this, &Form1::Form1_Load);
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
+			this->panel1->ResumeLayout(false);
+			this->panel1->PerformLayout();
 			this->ResumeLayout(false);
 
 		}
 #pragma endregion
+		private: Queue^ LoadList(Queue^ goods);
+		private: void AddEntry(int id, Queue^ goods, String^ name, double givenPrice);
+		private: Queue^ goods;
 	private: System::Void Form1_Load(System::Object^ sender, System::EventArgs^ e) {
 		
-
-		Good^ good = gcnew Good();
-		
-		good->Id = 1;
-		good->GoodName = "Õîëîäèëüíèê";
-		good->ActualPrice = 500;
-		good->GivenPrice = 550;
-		good->GivenDate = DateTime::Now;
-		
-	System:Queue^ goods = gcnew Queue();
-		goods->Enqueue(good);
 		int i = 0;
-		while (goods->Count) // 0 ïðèíèìàåòñÿ çà false
+		int count = goods->Count;
+		//dataGridView1->DataSource = NULL;
+		dataGridView1->Rows->Clear();
+
+		for each (Good^ good in goods)
 		{
-			Good^ g = safe_cast<Good^>(goods->Dequeue());
+			Good^ g = safe_cast<Good^>(good);
 
 			DataGridViewRow^ rowFullName = gcnew DataGridViewRow;
 			DataGridViewCell^ celFullName = gcnew DataGridViewTextBoxCell;
@@ -178,5 +246,12 @@ namespace CppCLRWinformsProjekt {
 			i++;
 		}
 	}
-	};
+
+		   int b = 1;
+	private: System::Void addNewEntryBtn_Click(System::Object^ sender, System::EventArgs^ e) 
+	{
+		AddEntry(b++, goods, InputNameTxt->Text, 100);
+		Form1_Load(sender,e);
+	}
+};
 }
